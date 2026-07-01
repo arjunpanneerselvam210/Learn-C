bac6d002ca1dca21ecc316391c856f190ec0f07d
arjunpanneerselvam210 <arjunpanneerselvam210@gmail.com>
2026-07-01 11:02:42 +0530

    LinkedList Problems -1 Added

---
+++ C Programming/Data Structures/Linked List/1. Insert After Every Even Node.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+typedef struct Node Node;
+
+Node *head = NULL;
+
+void display(){
+    Node *temp = head;
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+Node *createNode(int data){
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insert(Node *newNode){
+    if(head == NULL)
+        head = newNode;
+    else{
+        Node *temp = head;
+        while(temp.next != NULL)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void insertAfterEven(int even){
+    Node *temp = head;
+    while(temp != NULL ){
+        if(temp->next != NULL && temp->data % 2 == 0){
+            Node *dummy = createNode(even);
+            dummy->next = temp->next;
+            temp->next = dummy;
+        }
+        if(temp->next == NULL && temp->data % 2 == 0){
+            temp->next = createNode(even);
+        }
+    }
+}
+
+int main(){
+    while(1){
+        int data;
+        scanf("%d" , &data);
+        if(data == -1)
+            break;
+        insert(createNode(data));
+    }
+    display();
+    int even;
+    scanf("%d" , &even);
+    insertAfterEven(even);
+    display();
+    return 0;
+}
